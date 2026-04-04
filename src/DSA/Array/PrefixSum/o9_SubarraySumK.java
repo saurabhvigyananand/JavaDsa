978bd9425b3bb95d89ba8d876f41bdb920111b69
Saurabh Kumar Vigyananand <saurabhvigyananand@gmail.com>
2026-04-04 11:19:49 +0530

    Prefix countSubarrays to compute subarray sums

--- src/DSA/Array/PrefixSum/o9_SubarraySumK.java
+++ src/DSA/Array/PrefixSum/o9_SubarraySumK.java
@@ -68,19 +68,28 @@ public class o9_SubarraySumK {
 
     public static int countSubarrays(int[] nums, int k) {
 
-    	if(nums == null || nums.length==0)
-    		return 0;
-        // Write your code here
-    	
-    	Map<Integer, Integer> map = new HashMap<>();
-    	int count =0;
-    	int sum =0;
-    	map.put(0, 1);
-    	int j=0;
-    	for(int i=0;i<nums.length;i++) {
-    		
-    	}
-    	
-        return 0;
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int count = 0;
+        int sum = 0;
+
+        map.put(0, 1); // important
+
+        for (int i = 0; i < nums.length; i++) {
+
+            sum += nums[i];
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k); // FIX 1
+            }
+
+            // FIX 2: update frequency
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
     }
 }
